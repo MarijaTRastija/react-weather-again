32109bd7cc001450097302ba5770c9a66533abb3
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-08-26 11:00:14 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/marija-trzic-rastija-711009175/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Marija Tržić Rastija{" "}
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/MarijaTRastija/react-weather-again"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/marija-trzic-rastija-711009175/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Marija Tržić Rastija{" "}
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/MarijaTRastija/react-weather-again"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

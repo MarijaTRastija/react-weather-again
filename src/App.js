5a520cb7e910d06a3573db3386c167c7cdec81d7
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-08-26 10:49:00 +0200

    Added GitHub and Linkedin URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/marija-trzic-rastija-711009175/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Marija Tržić Rastija{" "}
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/MarijaTRastija/react-weather-again"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

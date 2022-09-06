2e359dfede45c1640283595bb26220a0120caf9d
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-09-06 11:01:43 +0200

    Added latitude and longitude api call

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [city, setCity] = useState(props.defaultCity);
   const [weatherData, setWeatherData] = useState({ ready: false });
   function handleResponse(response) {
+    console.log(response.data);
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       wind: Math.round(response.data.wind.speed),
       city: response.data.name,
@@ -20,7 +23,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "919fb7ba7dc6a5f22ab47490fe852f46";
+    const apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
 
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
@@ -59,6 +62,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
   } else {
